21804cbba7c6c8c0c988c896ed823ccf4cf2c650
Gregory Sarault <gregsarault@gmail.com>
2013-08-16 13:33:14 -0400

    adding files

---
+++ -/_inc/cssReference.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="/-/c/base.css" />
+    <!-- <script>document.write('<link rel="stylesheet" href="/-/c/enhanced.css" />');</script> -->
+<link href='http://fonts.googleapis.com/css?family=Lato|PT+Mono' rel='stylesheet' type='text/css'>
---
+++ index.php
@@ -0,0 +1,205 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
+<head>    
+    <title>Greg Sarault</title>
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?>        
+
+    <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>        
+
+</head>
+<body>
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <!-- ========= Intro ======== -->
+    <section class="intro clearfix">
+        <div class="inner">
+            <h1 class="intro-logo"><img class="intro-logo-image" src="data:image/svg+xml;base64, PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI0MTIuOTg5IiBoZWlnaHQ9IjY4LjY5NCIgdmlld0JveD0iMi44MDggOC4zNDcgNDEyLjk4OSA2OC42OTQiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMi44MDggOC4zNDcgNDEyLjk4OSA2OC42OTQiPjxwYXRoIGQ9Ik0zOC41MjMgNjIuNjR2LTUuMjU3aC0uMTQ0Yy0zLjA5NiA0LjAzMy02Ljk4NSA2LjI2NS0xMi45NjEgNi4yNjUtMTIuODE3IDAtMjIuNjEtOS40MzMtMjIuNjEtMjcuNTc4czguOTI5LTI3LjcyMyAyNC42OTgtMjcuNzIzYzEzLjAzMyAwIDE4Ljc5MyA2Ljg0MSAyMC45NTQgMTUuOTE0bC0xMy40NjUgMy4zODRjLTEuNTg0LTQuNzUyLTMuODE2LTYuOTEyLTcuOTkzLTYuOTEyLTYuNjI1IDAtOS41MDUgNS42MTYtOS41MDUgMTUuMjY1IDAgMTAuMjI2IDMuNDU2IDE1LjQ4MSA5LjU3NyAxNS40ODEgNS42ODggMCA4LjI4MS0zLjM4NCA5Ljg2NS03LjM0NWgtMTEuNjY1di0xMC44MDFoMjQuNDgydjI5LjMwN2gtMTEuMjMzek04MS45MzggMzcuNTgxYy00LjAzMiAwLTguMjA4IDEuMzY4LTExLjUyMSAzLjYwMXYyMS40NThoLTEzLjg5N3YtMzcuODA0aDEzLjg5N3Y3LjIwMWguMTQ0YzIuNTkyLTQuNjgxIDcuMDU3LTcuOTkzIDExLjM3Ny03Ljk5M2guMjE2djEzLjUzN2gtLjIxNnpNMTE5Ljc0NiA0Ni42NTRoLTIzLjExNGMuNTA0IDQuOTY5IDIuODggNi45ODQgNi45MTMgNi45ODQgMy4wOTYgMCA3LjA1Ny0xLjY1NiAxMC4zNjk
+            tNC4yNDhsNS42ODggNy40MTdjLTQuMjQ5IDMuODE1LTkuODY1IDYuNjI0LTE3LjEzOCA2LjYyNC0xMi44ODkgMC0xOS4wODItNy43NzYtMTkuMDgyLTE5LjY1NyAwLTEwLjgwMiA1LjQtMTkuNzMgMTguNzIyLTE5LjczIDEyLjA5NyAwIDE3Ljc4NiA5LjI4OSAxNy43ODYgMTkuMDgyIDAgMS40NC0uMDcyIDIuNDQ4LS4xNDQgMy41Mjh6bS0xOC4yMTctMTMuOTdjLTIuNTkyIDAtNC4zMiAxLjY1Ni00Ljg5NiA2LjQ4MWgxMC4wMDljLS40MzMtNC42MDgtMi4wMTctNi40ODEtNS4xMTMtNi40ODF6TTE1Ni4xMDUgMzQuNDEzYy4yMTYuODY0LjI4OCAxLjg3Mi4yODggMi44ODEgMCA5LTcuNDg5IDEzLjI0OS0xNi42MzMgMTMuMjQ5LTEuNjU2IDAtMy4zMTItLjE0NS00Ljc1Mi0uNDMzLS4zNi40MzMtLjY0OC45MzctLjY0OCAxLjU4NCAwIDEuMTUyLjkzNiAxLjUxMyAyLjk1MiAxLjU4NWw2LjEyMS41MDRjMTEuNTkzLjkzNiAxNy40MjUgNC4zMiAxNy40MjUgMTAuODAxIDAgOS4wNzItOC43MTMgMTIuNDU3LTIxLjMxNCAxMi40NTctMTIuODg5IDAtMTkuNzI5LTIuNTkzLTE5LjcyOS04LjI4MSAwLTMuMzEyIDIuMjMyLTYuMDQ4IDYuMTIxLTcuOTItMS4zNjgtMS4xNTItMi4xNi0yLjUyMS0yLjE2LTQuMDMyIDAtMy43NDUgMi4xNi02LjE5MyA2LjA0OC04LjQ5Ny00LjE3Ni0yLjE2LTYuNjI1LTUuOTA0LTYuNjI1LTExLjAxNyAwLTkuMDAxIDcuNTYxLTEzLjI1IDE2LjU2Mi0xMy4yNSA1LjExMiAwIDkuNzIxIDEuMzY4IDEyLj
+            Y3MyA0LjAzMyAyLjM3Ni0yLjQ0OCA1LjMyOS00LjAzMyA4LjY0MS00LjAzM3Y5Ljg2NWMtMS44MDEgMC0zLjYwMi4xNDQtNC45Ny41MDR6bS0xMy4zMjEgMzAuNjAzbC02LjEyLS40MzJjLTEuNTEyLS4xNDUtMi44OC0uMzYtNC4yNDktLjY0OC0uMjg4LjY0OC0uNTA0IDEuMjk2LS41MDQgMi4yMzIgMCAyLjIzMiAyLjQ0OCAzLjc0NCAxMC4wMDkgMy43NDQgNy4wNTcgMCA4Ljc4NS0xLjE1MiA4Ljc4NS0yLjU5MiAwLTEuMjk2LS45MzYtMS44MDEtNy45MjEtMi4zMDR6bS0zLjAyNC0zMy40MTJjLTIuNTIxIDAtNC4wMzIgMi4wMTctNC4wMzIgNS42ODlzMS41MTIgNS42ODggNC4wMzIgNS42ODhjMi42NjQgMCA0LjEwNC0yLjAxNyA0LjEwNC01LjY4OC4wMDEtMy42NzItMS40NC01LjY4OS00LjEwNC01LjY4OXpNMjAxLjc0MyA2My42NDdjLTEwLjE1MyAwLTE2Ljc3Ny0zLjI0LTIxLjg5LTkuNTA1bDkuOTM3LTguMTM3YzMuODE2IDQuMzIgNy40ODkgNi40OCAxMy4wMzMgNi40OCA0LjEwNSAwIDYuMjY1LTEuNjU1IDYuMjY1LTQuMzkyIDAtMi41MjEtMS42NTYtMy42NzMtNi40MDktNS4wNDEtMTQuNTQ1LTQuMDMyLTIxLjYwMi03LjkyMS0yMS42MDItMTguNDM0IDAtMTAuODAxIDkuMzYxLTE2LjI3MyAyMC4zNzgtMTYuMjczIDkuNjQ5IDAgMTUuODQxIDIuODA5IDIwLjY2NyA4LjY0MWwtOS44NjUgOC4yMDljLTMuMTY4LTMuNjcyLTYuNzY5LTUuNjg4LTEwLjk0NS01LjY4OC00LjAzMiAwLTUuNzYgMS41ODQtNS43NiAzL
+            jg4OSAwIDIuMTYgMS41MTIgMy40NTYgNi4zMzYgNC44MjQgMTYuNDE4IDQuNTM2IDIxLjYwMiA5LjE0NSAyMS42MDIgMTguNjQ5LS4wMDEgOC42NDItNi40MSAxNi43NzgtMjEuNzQ3IDE2Ljc3OHpNMjQ2LjU5OSA2Mi42NGMtLjUwNC0xLjE1Mi0uNzkyLTIuMjMyLTEuMDA4LTMuNTI4LTIuNzM2IDIuNTIxLTUuOTc3IDQuMzItMTAuNzI5IDQuMzItNi4wNDggMC0xMS4wMTctMy4xNjgtMTEuMDE3LTEwLjQ0IDAtOS41NzcgOS40MzMtMTMuODk3IDIxLjc0Ni0xNC45MDV2LS41MDVjMC0yLjU5Mi0xLjIyNS0zLjgxNS00LjgyNC0zLjgxNS0zLjI0IDAtNi4zMzcgMS4yOTYtOS42NDkgMy43NDRsLTYuMTItNy40ODljNC4wMzItMy42IDkuODY0LTUuOTc3IDE4LjA3My01Ljk3NyAxMC40NDEgMCAxNi4wNTggMy42MDEgMTYuMDU4IDE0LjMzdjE0Ljk3NWMwIDMuOTYxLjI4OCA3LjQxNyAxLjE1MiA5LjI4OWgtMTMuNjgyem0tMS4wMDgtMTcuNDI2Yy01LjU0NS42NDgtOC40MjUgMi43MzYtOC40MjUgNi4wNDkgMCAyLjAxNyAxLjA4IDMuNDU2IDMuMzEyIDMuNDU2IDEuNzI4IDAgMy42LS45MzYgNS4xMTItMi4zNzZ2LTcuMTI5ek0yODkuOTQ0IDM3LjU4MWMtNC4wMzIgMC04LjIwOSAxLjM2OC0xMS41MjEgMy42MDF2MjEuNDU4aC0xMy44OTZ2LTM3LjgwNGgxMy44OTZ2Ny4yMDFoLjE0NWMyLjU5Mi00LjY4MSA3LjA1Ny03Ljk5MyAxMS4zNzctNy45OTNoLjIxNnYxMy41MzdoLS4yMTd6TTMxMi41NSA2Mi42NGMtLjUwNC0xLjE1
+            Mi0uNzkyLTIuMjMyLTEuMDA4LTMuNTI4LTIuNzM2IDIuNTIxLTUuOTc3IDQuMzItMTAuNzI5IDQuMzItNi4wNDggMC0xMS4wMTctMy4xNjgtMTEuMDE3LTEwLjQ0IDAtOS41NzcgOS40MzMtMTMuODk3IDIxLjc0Ni0xNC45MDV2LS41MDVjMC0yLjU5Mi0xLjIyNS0zLjgxNS00LjgyNC0zLjgxNS0zLjI0IDAtNi4zMzcgMS4yOTYtOS42NDkgMy43NDRsLTYuMTItNy40ODljNC4wMzItMy42IDkuODY0LTUuOTc3IDE4LjA3My01Ljk3NyAxMC40NDEgMCAxNi4wNTggMy42MDEgMTYuMDU4IDE0LjMzdjE0Ljk3NWMwIDMuOTYxLjI4OCA3LjQxNyAxLjE1MiA5LjI4OWgtMTMuNjgyem0tMS4wMDgtMTcuNDI2Yy01LjU0NS42NDgtOC40MjUgMi43MzYtOC40MjUgNi4wNDkgMCAyLjAxNyAxLjA4IDMuNDU2IDMuMzEyIDMuNDU2IDEuNzI4IDAgMy42LS45MzYgNS4xMTItMi4zNzZ2LTcuMTI5ek0zNTIuNzI2IDYyLjY0di00LjYwOGMtNC4yNDkgMy4zODUtOC4xMzcgNS40LTEyLjg5IDUuNC01LjY4OCAwLTguNTY4LTMuMDk3LTguNTY4LTkuNjQ4di0yOC45NDhoMTMuODk3djI1LjI3NGMwIDEuNTg0LjU3NiAyLjUyMSAyLjA4OCAyLjUyMXMzLjI0LS45MzcgNS40NzMtMi41MjF2LTI1LjI3NGgxMy44OTd2MzcuODA0aC0xMy44OTd6TTM3My4xIDYyLjY0di01My4yODVoMTMuODk3djUzLjI4NWgtMTMuODk3ek00MDcuODc3IDYzLjE0M2MtOC4xMzcgMC0xMy4xMDUtMy4yNC0xMy4xMDUtMTEuOTUzdi0xNi44NWgtMy45NnYtOS41MDVoMy4
+            5NnYtMTIuMzg0aDEzLjgyNXYxMi4zODVoNi44NDF2OS41MDVoLTYuODQxdjE0LjgzNGMwIDIuOTUyIDEuMzY4IDQuMTA0IDMuNjcyIDQuMTA0IDEuNTEzIDAgMi41OTMtLjA3MiAzLjUyOC0uMjg4djkuNzIxYy0xLjcyNy4yODgtNC4wMzIuNDMxLTcuOTIuNDMxeiIvPjwvc3ZnPg==" alt="Greg Sarault"/></h1>
+            <h2 class="intro-pitch">Web Developer for hire</h2>
+            <p class="intro-slogan">Former developer apprentice at <a href="superfriend.ly" class="intro-link">SuperFriendly</a>. Jack of all trades, Master of some.</p>
+
+            <p class="intro-mail"><a href="mailto:greg@gmail.com" class="intro-mail-link">Let's Talk</a></p>
+        </div><!-- .inner -->    
+    </section><!-- .intro -->
+
+
+    <!-- ========= Banner (Pixelingo) LEFT TAB ======== -->
+    <section class="banner pixelingo oscillate-left-tab">
+        <div class="inner">
+            <div class="banner-left">
+                <div class="banner-innards">
+                    <h1 class="banner-heading">Pixelingo</h1>
+                    <p class="banner-content">Really sweet and simple site, I was able to practice sizing font for every size of screen, and did some amazing JavaScripting with the navigation.</p>
+                    <p class="banner-site"><a href="pixelingo.com" class="banner-site-link">Pixelingo.com</a></p>
+                    <ul class="banner-skills">
+                        <li class="banner-skills-feature">HTML</li>
+                        <li class="banner-skills-feature">CSS3</li>
+                        <li class="banner-skills-feature">JS(Jquery)</li>
+                        <li class="banner-skills-feature">Math</li>
+                        <li class="banner-skills-feature">Physics</li>
+                    </ul><!-- .banner-skills -->
+                </div><!-- .banner-innards -->
+                <p class="banner-more"><a href="/pixlingo" class="banner-more-link">See the code</a></p>
+            </div><!-- .banner-left -->
+            <div class="banner-right">
+                <img class="banner-bomb" src="http://placebear.com/500/465" alt="Pixelingo" />
+            </div><!-- .baner-right -->
+        </div><!-- .inner -->
+    </section><!-- .banner .pixelingo -->
+
+
+    <!-- ========= Banner (NewsWorks) RIGHT TAB no image ======== -->
+
+    <section class="banner newsworks oscillate-right-tab ">
+        <div class="inner">
+            <h1 class="banner-heading">NewsWorks</h1>
+            <p class="banner-content">Working with Kevin Hoffmen - I Used Twitter-BootStrap and also Practicing SMACCS and Harry Roberts methods of CSS selector intent.</p>
+            <p class="banner-site">Not yet live</p>
+            <ul class="banner-skills">
+                <li class="banner-skills-feature">HTML</li>
+                <li class="banner-skills-feature">CSS3</li>
+                <li class="banner-skills-feature">JS(Jquery)</li>
+                <li class="banner-skills-feature">interpretation</li>
+                <li class="banner-skills-feature">Ping Pong</li>
+            </ul><!-- .banner-skills -->
+            <p class="banner-more"><a href="/newsworks">See the code</a></p>
+        </div><!-- .inner -->
+    </section><!-- .banner .pixelingo -->
+
+
+
+    <!-- ========= Banner (Carolina Painting) LEFT TAB ======== -->
+
+    <section class="banner cp oscillate-left-tab">
+        <div class="inner">
+            <div class="banner-left">
+                <div class="banner-innards">
+                    <h1 class="banner-heading">Carolina Painting</h1>
+                    <p class="banner-content">Building this site with creative plugins and a some function oriented Java Script. Developed both Front and Back-end.</p>
+                    <p class="banner-site"><a href="carolinapainting.com" class="banner-site-link">CarolinaPainting.com</a></p>
+                    <ul class="banner-skills">
+                        <li class="banner-skills-feature">HTML</li>
+                        <li class="banner-skills-feature">CSS3</li>
+                        <li class="banner-skills-feature">JS(Jquery)</li>
+                        <li class="banner-skills-feature">EE</li>
+                        <li class="banner-skills-feature"></li>
+                    </ul><!-- .banner-skills -->
+                </div><!-- .banner-innards -->
+                <p class="banner-more"><a href="/cp" class="banner-more-link">See the code</a></p>
+            </div><!-- .banner-left -->
+            <div class="banner-right">
+                <img class="banner-bomb" src="http://placebear.com/501/465" alt="Carolina Painting" />
+            </div><!-- .banner-right -->
+        </div><!-- .inner -->
+    </section><!-- .banner .pixelingo -->
+
+    <!-- ========= Banner (TC) RIGHT TAB no image ======== -->
+
+    <section class="banner tc oscillate-right-tab">
+        <div class="inner">
+            <h1 class="banner-heading"><span class="strike">%$%$ %$*$*$</span></h1>
+            <p class="banner-content">Worked with Brad Frost on the redesign of <span class="strike">%$%$ %$*$*$</span>. Learned Sass over night, and used/test Brad Frost's <a href="http://patternlab.bradfrostweb.com/">Patterns Lab</a>before he release it.</p>
+            <p class="banner-site">Not yet live</p>
+            <ul class="banner-skills">
+                <li class="banner-skills-feature">Sass</li>
+                <li class="banner-skills-feature">Gradients</li>
+                <li class="banner-skills-feature">SVG's</li>
+                <li class="banner-skills-feature"></li>
+                <li class="banner-skills-feature">Patterns</li>
+            </ul><!-- .banner-skills -->
+            <p class="banner-more"><a href="/blank" class="banner-more-link">See the code</a></p>
+        </div><!-- .inner -->
+    </section><!-- .banner .pixelingo -->
+
+    <!-- ========= Banner (BearSkinRug) LEFT TAB ======== -->
+    
+    <section class="banner bear oscillate-left-tab">
+        <div class="inner">
+            <div class="banner-left">
+                <div class="banner-innards">
+                    <h1 class="banner-heading">BearSkinRug</h1>
+                    <p class="banner-content">Towards the very end of this project, I was called in to hack my way through an already built <abbr title="Content Management System">CMS</abbr> (WordPress). Making extream adjustment across this site.</p>
+                    <p class="banner-site"><a href="bearskinrug.co.uk" class="banner-site-link">bearskinrug.co.uk</a></p>
+                    <ul class="banner-skills">
+                        <li class="banner-skills-feature">PHP</li>
+                        <li class="banner-skills-feature">CSS3</li>
+                        <li class="banner-skills-feature">Conditionals</li>
+                        <li class="banner-skills-feature">Bear Fighting</li>
+                        <li class="banner-skills-feature">Magic</li>
+                    </ul><!-- .banner-skills -->
+                </div><!-- .banner-innards -->
+                <p class="banner-more"><a href="/bearskin" class="banner-more-link">See the code</a></p>
+            </div><!-- .banner-left -->
+            <div class="banner-right">
+                <img class="banner-bomb" src="http://placebear.com/499/465" alt="BearSkinRug" />
+            </div><!-- .banner-right -->
+        </div><!-- .inner -->
+    </section><!-- .banner .pixelingo -->
+
+
+    <!-- ========= Banner (Typedia) RIGHT TAB no image ======== -->
+
+    <section class="banner typedia oscillate-right-tab">
+        <div class="inner">
+            <h1 class="banner-heading">Typedia</h1>
+            <p class="banner-content">Weekly meeting's with Jason Santa Maria, Dave Dawson, Dan Mall, and Vector (Featuring Matt Weinberg). Brought on to this project to learn, and ended up taking on as much as I could to get this project finished.</p>
+            <p class="banner-site">Not yet live</p>
+            <ul class="banner-skills">
+                <li class="banner-skills-feature">HTML</li>
+                <li class="banner-skills-feature">CSS3</li>
+                <li class="banner-skills-feature">JS(Jquery)</li>
+                <li class="banner-skills-feature">Canvas</li>
+                <li class="banner-skills-feature">Ajax</li>
+            </ul><!-- .banner-skills -->
+            <p class="banner-more"><a href="/typedia" class="banner-more-link">See the code</a></p>
+        </div><!-- .inner -->
+    </section><!-- .banner .pixelingo -->
+
+
+    <!-- ========= Banner (Businessology) LEFT TAB ======== -->
+    
+    <section class="banner business oscillate-left-tab">
+        <div class="inner">
+            <div class="banner-left">
+                <div class="banner-innards">
+                    <h1 class="banner-heading">The Businessology Show</h1>
+                    <p class="banner-content">My very first project. I built the front-end first, integrated in to WordPress later. Then REBUILT it, and Integrated it with Expression Engine. </p>
+                    <p class="banner-site"><a href="thebusinessolgyshow.biz" class="banner-site-link">thebusinessolgyshow.biz</a></p>
+                    <ul class="banner-skills">
+                        <li class="banner-skills-feature">HTML</li>
+                        <li class="banner-skills-feature">CSS3</li>
+                        <li class="banner-skills-feature">WordPress</li>
+                        <li class="banner-skills-feature">Expression Engine</li>
+                        <li class="banner-skills-feature">Repatition</li>
+                    </ul><!-- .banner-skills -->
+                </div><!-- .banner-innards -->
+                <p class="banner-more"><a href="/business" class="banner-more-link">See the code</a></p>
+            </div><!-- .banner-left -->
+            <div class="banner-right">
+                <img class="banner-bomb" src="http://placebear.com/500/466" alt="The Businessology Show" />
+            </div><!-- .banner-right -->
+        </div><!-- .inner -->
+    </section><!-- .banner .pixelingo -->
+
+
+    <!-- ========= Banner Footer ======== -->
+
+    <section class="footer">
+        <div class="inner">
+            <h1 class="footer-email"><a href="mailto:gregsarault@gmail.com">GregSarault@gmail.com</a></h1>
+            <p class="footer-twitter">GregSarault</p>
+            <p class="copyright">2013</p>
+
+
+        </div><!-- .inner -->    
+    </section><!-- .footer -->
+
+
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
